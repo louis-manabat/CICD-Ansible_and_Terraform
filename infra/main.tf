a2c1c67991a6081a11977606fb72a0c24921a6ee
s3719633 <s3719633@student.rmit.edu.au>
2021-05-09 23:53:11 +1000

    [Task] Added security group
    Security group allows inbound traffic for HTTP and SSH

--- infra/main.tf
+++ infra/main.tf
@@ -25,3 +25,35 @@ resource "aws_security_group" "dbsc" {
     Name = "allow-docdb-traffic"
   }
 }
+
+resource "aws_security_group" "allow_http_ssh" {
+  description = "Allowing HTTP and SSH inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "HTTP from internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH from internet"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_http_ssh"
+  }
+}
