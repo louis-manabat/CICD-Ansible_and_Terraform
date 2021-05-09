3864f1bdfd33c9282cc4dfc4c6ec2dcd9db957ef
s3719633 <s3719633@student.rmit.edu.au>
2021-05-09 23:02:32 +1000

    [Task] Got IGW creation working
    IGW = Internet Gateways

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -1,7 +1,15 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "s3719633-a2-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "s3719633-a2-igw"
   }
 }
