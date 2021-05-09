4dda08cd9d188471a7f066310fdbc0a3a4437f3d
s3719633 <s3719633@student.rmit.edu.au>
2021-05-09 23:07:27 +1000

    [Task] Got Subnet creation working
    Got subnet creation for public, private and data availability zones done

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -26,3 +26,102 @@ resource "aws_default_route_table" "drt" {
     Name = "s3719633-a2-drt"
   }
 }
+
+resource "aws_subnet" "public_az1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.0.0/22"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "a2-public-az1"
+  }
+}
+
+resource "aws_subnet" "public_az2" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.4.0/22"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "a2-public-az2"
+  }
+}
+
+resource "aws_subnet" "public_az3" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.8.0/22"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "a2-public-az3"
+  }
+}
+
+resource "aws_subnet" "private_az1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.16.0/22"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "a2-private-az1"
+  }
+}
+
+resource "aws_subnet" "private_az2" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.20.0/22"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "a2-private-az2"
+  }
+}
+
+resource "aws_subnet" "private_az3" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.24.0/22"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "a2-private-az3"
+  }
+}
+
+resource "aws_subnet" "data_az1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.32.0/22"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "a2-data-az1"
+  }
+}
+
+resource "aws_subnet" "data_az2" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.36.0/22"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "a2-data-az2"
+  }
+}
+
+resource "aws_subnet" "data_az3" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.40.0/22"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "a2-data-az3"
+  }
+}
