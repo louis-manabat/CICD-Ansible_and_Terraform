4add2634cc4a0790ccd8ba08876f9ef1009f009e
s3719633 <s3719633@student.rmit.edu.au>
2021-05-09 23:06:13 +1000

    [Task] Got DRT working
    DRT = Default Route Table

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -13,3 +13,16 @@ resource "aws_internet_gateway" "igw" {
     Name = "s3719633-a2-igw"
   }
 }
+
+resource "aws_default_route_table" "drt" {
+  default_route_table_id = aws_vpc.vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "s3719633-a2-drt"
+  }
+}
