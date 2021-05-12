6ca5ef5efa47c2011fdae27f39917a683ff37b41
s3719633 <s3719633@student.rmit.edu.au>
2021-05-12 12:58:31 +1000

    [Task] Added public IP of EC2 instance

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,9 @@
 output "endpoint" {
-  value = aws_lb.todo_app_lb.dns_name
-} 
+  description = "URL of the EC2 instance"
+  value       = aws_lb.todo_app_lb.dns_name
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.todo_app_ec2.public_ip
+}
