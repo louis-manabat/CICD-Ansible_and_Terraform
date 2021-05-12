563223cf2e81e067fa11998cbca8b55f48e807ae
s3719633 <s3719633@student.rmit.edu.au>
2021-05-12 15:02:20 +1000

    [Task] Added array identifier due to ec2.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -5,5 +5,5 @@ output "endpoint" {
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.todo_app_ec2.public_ip
+  value       = aws_instance.todo_app_ec2[0].public_ip
 }
