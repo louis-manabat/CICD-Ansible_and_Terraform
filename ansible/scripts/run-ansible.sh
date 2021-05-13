8b448f068e94b7eb9fccb8c9e24d7bde7a45902f
s3719633 <s3719633@student.rmit.edu.au>
2021-05-13 20:56:06 +1000

    [Task] Outputted some docdb info

--- ansible/scripts/run-ansible.sh
+++ ansible/scripts/run-ansible.sh
@@ -5,3 +5,5 @@ set +ex
 echo "all:" > inventory.yml
 echo "  hosts:" >> inventory.yml
 echo "    "$(cd ../../infra && terraform output instance_public_ip) >> inventory.yml
+
+## Database stuff
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -7,3 +7,19 @@ output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.todo_app_ec2[0].public_ip
 }
+
+output "docdb_endpoint" {
+  description = "The DNS address for the DocDB instance"
+  value       = aws_docdb_cluster.docodb.endpoint
+}
+
+output "docdb_username" {
+  description = "The username for the DocDB instance"
+  value       = aws_docdb_cluster.docodb.master_username
+}
+
+output "docdb_password" {
+  sensitive   = true
+  description = "The password for the DocDB instance"
+  value       = aws_docdb_cluster.docodb.master_password
+}
