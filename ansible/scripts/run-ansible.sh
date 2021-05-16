fde5bfc6a2d9e8eb4ae7a92e3de065ed1c0b7784
s3719633 <s3719633@student.rmit.edu.au>
2021-05-17 00:22:10 +1000

    [Task] Added database vars to ansible script

--- ansible/scripts/run-ansible.sh
+++ ansible/scripts/run-ansible.sh
@@ -7,7 +7,10 @@ echo "  hosts:" >> inventory.yml
 echo "    "$(cd ../../infra && terraform output instance_public_ip) >> inventory.yml
 
 ## Database stuff
-
+echo "  vars:" >> inventory.yml
+echo "    docdb_endpoint="$(cd ../../infra && terraform output docdb_endpoint) >> inventory.yml
+echo "    docdb_username="$(cd ../../infra && terraform output docdb_username) >> inventory.yml
+echo "    docdb_password="$(cd ../../infra && terraform output docdb_password) >> inventory.yml
 
 ## Running ansible-playbook
 # ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -i scripts/inventory.yml -u ec2-user playbook.yml --private-key ~/keys/ec2-key
