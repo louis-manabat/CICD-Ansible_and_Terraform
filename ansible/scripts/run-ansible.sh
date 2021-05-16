2dc806e144d6b35ca2b805e5cff43e4286ea2c08
s3719633 <s3719633@student.rmit.edu.au>
2021-05-16 20:21:01 +1000

    [Task] Added ansible-playbook command
    Commented it out until testing is completed

--- ansible/scripts/run-ansible.sh
+++ ansible/scripts/run-ansible.sh
@@ -7,3 +7,7 @@ echo "  hosts:" >> inventory.yml
 echo "    "$(cd ../../infra && terraform output instance_public_ip) >> inventory.yml
 
 ## Database stuff
+
+
+## Running ansible-playbook
+# ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -i scripts/inventory.yml -u ec2-user playbook.yml --private-key ~/keys/ec2-key
