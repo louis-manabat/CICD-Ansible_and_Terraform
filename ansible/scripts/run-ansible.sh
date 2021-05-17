0bc4f0012f874d1e379cc6df025a80595de97ee8
s3719633 <s3719633@student.rmit.edu.au>
2021-05-17 11:57:44 +1000

    [Task] Got the docdb env url sorted

--- ansible/scripts/run-ansible.sh
+++ ansible/scripts/run-ansible.sh
@@ -7,10 +7,12 @@ echo "  hosts:" >> inventory.yml
 echo "    "$(cd ../infra && terraform output instance_public_ip) >> inventory.yml
 
 ## Database stuff
-# echo "  vars:" >> inventory.yml
-# echo "    docdb_endpoint="$(cd ../../infra && terraform output docdb_endpoint) >> inventory.yml
-# echo "    docdb_username="$(cd ../../infra && terraform output docdb_username) >> inventory.yml
-# echo "    docdb_password="$(cd ../../infra && terraform output docdb_password) >> inventory.yml
+docdb_endpoint=$(cd ../infra && terraform output docdb_endpoint | tr -d '"')
+docdb_username=$(cd ../infra && terraform output docdb_username | tr -d '"')
+docdb_password=$(cd ../infra && terraform output docdb_password | tr -d '"')
+docdb_port=$(cd ../infra && terraform output docdb_port)
+
+docdb_url="mongodb://$docdb_username:$docdb_password@$docdb_endpoint:$docdb_port/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
 
 rm -f templates/service.tpl
 
@@ -21,7 +23,7 @@ Requires=network-online.target
 After=network-online.target
 
 [Service]
-Environment=DB_URL="password" "assignment2-app.cluster..." "5432" from terraform output tute 5
+Environment="DB_URL=$docdb_url"
 Environment=SESSION_SECRET=secret
 WorkingDirectory=
 Type=simple
--- ansible/templates/service.tpl
+++ ansible/templates/service.tpl
@@ -4,12 +4,9 @@ Requires=network-online.target
 After=network-online.target
 
 [Service]
-Environment=DB_URL=
+Environment="DB_URL=mongodb://mongousername:mongopassword@todo-app.cluster-ciwgmikiqe2t.us-east-1.docdb.amazonaws.com:5432/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
 Environment=SESSION_SECRET=secret
 WorkingDirectory=
 Type=simple
 ExecStart=
 Restart=on-failure
-
-[Install]
-WantedBy=multi-user.target
