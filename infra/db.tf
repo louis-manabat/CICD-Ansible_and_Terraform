bb5ef00a8d232f0bca01441bf899c444df397c1f
s3719633 <s3719633@student.rmit.edu.au>
2021-05-18 22:19:42 +1000

    [Format] Formatted db.tf to align all the '='

--- infra/db.tf
+++ infra/db.tf
@@ -3,14 +3,14 @@ resource "aws_db_subnet_group" "dbs" {
 }
 
 resource "aws_docdb_cluster" "docodb" {
-  cluster_identifier      = "todo-app"
-  engine                  = "docdb"
-  master_username         = "mongousername"
-  master_password         = "mongopassword"
-  port                    = 5432
-  backup_retention_period = 1
-  skip_final_snapshot     = true
-  vpc_security_group_ids  = [aws_security_group.dbsc.id]
-  db_subnet_group_name    = aws_db_subnet_group.dbs.name
+  cluster_identifier              = "todo-app"
+  engine                          = "docdb"
+  master_username                 = "mongousername"
+  master_password                 = "mongopassword"
+  port                            = 5432
+  backup_retention_period         = 1
+  skip_final_snapshot             = true
+  vpc_security_group_ids          = [aws_security_group.dbsc.id]
+  db_subnet_group_name            = aws_db_subnet_group.dbs.name
   db_cluster_parameter_group_name = "tls-disable"
 }
