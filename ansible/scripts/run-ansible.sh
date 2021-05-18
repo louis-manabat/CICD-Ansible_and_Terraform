fc2da79a0d78480b1ec6bf9f57e8ef43b3760619
s3719633 <s3719633@student.rmit.edu.au>
2021-05-18 22:18:35 +1000

    [Task] Updated inventory file and fixed ansible command
    Added an extra directory to direct to the inventory file and made the ansible-playbook command a lot more readable

--- ansible/scripts/run-ansible.sh
+++ ansible/scripts/run-ansible.sh
@@ -2,9 +2,15 @@
 set +ex
 
 ## Hacky way to build inventory file
-echo "all:" > inventory.yml
-echo "  hosts:" >> inventory.yml
-echo "    "$(cd ../infra && terraform output instance_public_ip) >> inventory.yml
+echo "all:" > scripts/inventory.yml
+echo "  hosts:" >> scripts/inventory.yml
+echo "    "$(cd ../infra && terraform output instance_public_ip) >> scripts/inventory.yml
 
 ## Running ansible-playbook and parsing docdb variables
-# ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -i scripts/inventory.yml -u ec2-user playbook.yml --private-key ~/keys/ec2-key -e db_endpoint=$(cd ../infra && terraform output docdb_endpoint | tr -d '"') db_user=$(cd ../infra && terraform output docdb_username | tr -d '"') db_password=docdb_password=$(cd ../infra && terraform output docdb_password | tr -d '"') db_port=$(cd ../infra && terraform output docdb_port)
+ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -i scripts/inventory.yml \
+                                                -u ec2-user playbook.yml \
+                                                --private-key ~/keys/ec2-key \
+                                                -e db_endpoint=$(cd ../infra && terraform output docdb_endpoint | tr -d '"') \
+                                                -e db_user=$(cd ../infra && terraform output docdb_username | tr -d '"') \
+                                                -e db_password=docdb_password=$(cd ../infra && terraform output docdb_password | tr -d '"') \
+                                                -e db_port=$(cd ../infra && terraform output docdb_port)
