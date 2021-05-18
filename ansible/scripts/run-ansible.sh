d6074a48501320ca0345f67b93fd1a1aefb1614a
s3719633 <s3719633@student.rmit.edu.au>
2021-05-19 00:08:35 +1000

    [Refactor]

--- ansible/scripts/run-ansible.sh
+++ ansible/scripts/run-ansible.sh
@@ -11,6 +11,6 @@ ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -i scripts/inventory.yml \
                                                 -u ec2-user playbook.yml \
                                                 --private-key ~/keys/ec2-key \
                                                 -e db_endpoint=$(cd ../infra && terraform output docdb_endpoint | tr -d '"') \
-                                                -e db_user=$(cd ../infra && terraform output docdb_username | tr -d '"') \
-                                                -e db_password=docdb_password=$(cd ../infra && terraform output docdb_password | tr -d '"') \
+                                                -e db_username=$(cd ../infra && terraform output docdb_username | tr -d '"') \
+                                                -e db_password=$(cd ../infra && terraform output docdb_password | tr -d '"') \
                                                 -e db_port=$(cd ../infra && terraform output docdb_port)
