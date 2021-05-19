30888621ce6f2ebed37afeaa527d2d7e22e3786b
s3719633 <s3719633@student.rmit.edu.au>
2021-05-19 23:55:40 +1000

    [Task] Moved http/ssh sec group into ec2.tf and split them into 2 separate resources

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -3,11 +3,63 @@ resource "aws_key_pair" "deployer" {
   public_key = file("~/keys/ec2-key.pub")
 }
 
+resource "aws_security_group" "allow_http" {
+  description = "Allowing HTTP traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "HTTP from internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_http_ssh"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  description = "Allowing SSH traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "SSH from internet"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allowing port 5000"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.vpc.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_lb" "todo_app_lb" {
   name               = "todo-app-lb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.allow_http_ssh.id] # update this sec group
+  security_groups    = [aws_security_group.allow_http.id]
   subnets            = [aws_subnet.public_az1.id, aws_subnet.public_az2.id, aws_subnet.public_az3.id]
 
   tags = {
@@ -39,7 +91,7 @@ resource "aws_instance" "todo_app_ec2" {
   subnet_id                   = aws_subnet.private_az1.id
   associate_public_ip_address = true
   key_name                    = aws_key_pair.deployer.key_name
-  security_groups             = [aws_security_group.allow_http_ssh.id] # update this sec group
+  security_groups             = [aws_security_group.allow_ssh.id]
   count                       = 1
 
   tags = {
