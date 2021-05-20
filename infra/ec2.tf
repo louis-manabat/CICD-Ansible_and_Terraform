1d1eaae756395cfa482998710ea75877b1152654
s3719633 <s3719633@student.rmit.edu.au>
2021-05-21 00:43:16 +1000

    [Task] Added/fixed tags for security groups

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -23,7 +23,7 @@ resource "aws_security_group" "allow_http" {
   }
 
   tags = {
-    Name = "allow_http_ssh"
+    Name = "allow_http"
   }
 }
 
@@ -52,6 +52,10 @@ resource "aws_security_group" "allow_ssh" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
   }
 }
 
