f7a3d522ed99a3fcd04b17ce9fcaca0047961c59
s3719633 <s3719633@student.rmit.edu.au>
2021-05-21 21:49:17 +1000

    [Refactor] Aligned the parameters for readability

--- ansible/scripts/run-ansible.sh
+++ ansible/scripts/run-ansible.sh
@@ -8,9 +8,9 @@ echo "    "$(cd ../infra && terraform output instance_public_ip) >> inventory.ym
 
 ## Running ansible-playbook and parsing docdb variables
 ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -i inventory.yml \
-                                                -u ec2-user playbook.yml \
-                                                --private-key ~/keys/ec2-key \
-                                                -e db_endpoint=$(cd ../infra && terraform output docdb_endpoint | tr -d '"') \
-                                                -e db_username=$(cd ../infra && terraform output docdb_username | tr -d '"') \
-                                                -e db_password=$(cd ../infra && terraform output docdb_password | tr -d '"') \
-                                                -e db_port=$(cd ../infra && terraform output docdb_port)
+                                                 -u ec2-user playbook.yml \
+                                                 --private-key ~/keys/ec2-key \
+                                                 -e db_endpoint=$(cd ../infra && terraform output docdb_endpoint | tr -d '"') \
+                                                 -e db_username=$(cd ../infra && terraform output docdb_username | tr -d '"') \
+                                                 -e db_password=$(cd ../infra && terraform output docdb_password | tr -d '"') \
+                                                 -e db_port=$(cd ../infra && terraform output docdb_port)
